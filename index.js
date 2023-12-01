18f07b03d3662d649504cb16bef6346dd97f0f3f
maximentor <maximentor@naver.com>
2023-12-01 15:17:42 +0900

    index.js function modify

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 /*
     index.html을 위한 js 파일
 */
-function add(a,b) {
+function add(a,b,c) {
+    if (c !== undefined){
+        return a+b;
+    }
     return a+b;
+}
+function substract(a,b){
+    if (a > b) {
+        return a-b;
+    }
+    else {
+        return b-a;
+    }
 }
