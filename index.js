feaf2c46c4895a424af31bd05f23f3c69c1d66fa
maximentor <maximentor@naver.com>
2023-12-01 15:25:34 +0900

    test!!

--- index.js
+++ index.js
@@ -1,6 +1,26 @@
 /*
     index.html을 위한 js 파일
 */
-function add(a,b) {
+function add(a,b,c) {
+    if (c !== undefined){
+        return a+b;
+    }
     return a+b;
+}
+function substract(a,b){
+    if (a > b) {
+        return a-b;
+    }
+    else {
+        return b-a;
+    }
+function add(a,b, c=10) {
+    if (c !== undefined){
+        return a+b+c;
+    }
+return a+b;
+}
+
+function multiply(a,b){
+    return a*b;
 }
